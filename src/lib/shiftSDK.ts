24c20316e81aefcc80204c6359608e65f9046a5b
noiach <gh@noiach.com>
2019-05-10 15:57:04 +1000

    Fixed getReserveBalance

--- src/lib/shiftSDK.ts
+++ src/lib/shiftSDK.ts
@@ -8,9 +8,9 @@ import { MarketPair, Token } from "../store/types/general";
 import { EXCHANGE, INFURA_URL } from "./environmentVariables";
 
 import { ERC20DetailedWeb3 } from "./contracts/erc20";
-import * as ERC20ABI from "./contracts/erc20_abi.json";
+const ERC20ABI = require("./contracts/erc20_abi.json");
 import { RenExWeb3 } from "./contracts/ren_ex";
-import * as RenExABI from "./contracts/ren_ex_abi.json";
+const RenExABI = require("./contracts/ren_ex_abi.json");
 
 interface Commitment {
     srcToken: string;
@@ -58,7 +58,7 @@ interface ShiftSDK {
 
 const getWeb3 = () => new Web3(INFURA_URL);
 const getExchange = (web3?: Web3): RenExWeb3 =>
-    new ((web3 || getWeb3()).eth.Contract)(RenExABI as AbiItem[], EXCHANGE);
+    new ((web3 || getWeb3()).eth.Contract)(RenExABI as AbiItem[], "0x0dF3510a4128c0cA11518465f670dB970E9302B7");
 const getERC20 = (tokenAddress: string, web3?: Web3): ERC20DetailedWeb3 =>
     new ((web3 || getWeb3()).eth.Contract)(ERC20ABI as AbiItem[], tokenAddress);
 
@@ -77,10 +77,12 @@ const getReserveBalance = async (marketPairs: MarketPair[]): Promise<ReserveBala
         }
         const tokenAddress = kovanAddresses.addresses.tokens[token].address;
         const tokenInstance = getERC20(tokenAddress, web3);
-        return new BigNumber((await tokenInstance.methods.balanceOf(address).call()).toString());
+        const decimals = kovanAddresses.addresses.tokens[token].decimals;
+        const rawBalance = new BigNumber((await tokenInstance.methods.balanceOf(address).call()).toString());
+        return rawBalance.dividedBy(new BigNumber(10).exponentiatedBy(decimals));
     };
 
-    return Promise.all(
+    return await Promise.all(
         marketPairs.map(async (_marketPair) => {
             const [left, right] = _marketPair.split("/") as [Token, Token];
             const leftAddress = kovanAddresses.addresses.tokens[left].address;
@@ -88,7 +90,7 @@ const getReserveBalance = async (marketPairs: MarketPair[]): Promise<ReserveBala
             const reserve = await exchange.methods.reserve(leftAddress, rightAddress).call();
             const leftBalance = await balance(left, reserve);
             const rightBalance = await balance(right, reserve);
-            return new Map([[left, leftBalance], [right, rightBalance]]);
+            return new Map().set(left, leftBalance).set(right, rightBalance);
         })
     );
 };
