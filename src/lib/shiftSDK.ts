83551ec42ab0aae21fa58e857a99300f06875c08
noiach <gh@noiach.com>
2019-05-10 14:43:51 +1000

    Updated getReserveBalance interface

--- src/lib/shiftSDK.ts
+++ src/lib/shiftSDK.ts
@@ -2,7 +2,7 @@ import BigNumber from "bignumber.js";
 import Web3 from "web3";
 
 import { AbiItem } from "web3-utils";
-import { MarketPair } from "../store/types/general";
+import { MarketPair, Token } from "../store/types/general";
 import { EXCHANGE, INFURA_URL } from "./environmentVariables";
 
 import { ERC20DetailedWeb3 } from "./contracts/erc20";
@@ -46,25 +46,40 @@ type ShiftDetails = {
  * @interface ShiftSDK
  */
 interface ShiftSDK {
-    getReserveBalance(marketPairs: MarketPair[]): Promise<BigNumber[]>;
+    getReserveBalance(marketPairs: MarketPair[]): Promise<Array<Map<Token, BigNumber>>>;
     submitCommitment(commitment: Commitment): Promise<ShiftDetails>;
     getCommitmentStatus(commitmentHash: string): Promise<ShiftDetails>;
 }
 
+/// Initialize Web3 and contracts
+
 const getWeb3 = () => new Web3(INFURA_URL);
-const getExchange = (web3?: Web3) => new ((web3 || getWeb3()).eth.Contract)(RenExABI as AbiItem[], EXCHANGE);
-const getERC20 = (tokenAddress: string, web3?: Web3) => new ((web3 || getWeb3()).eth.Contract)(ERC20ABI as AbiItem[], tokenAddress);
+const getExchange = (web3?: Web3): RenExWeb3 =>
+    new ((web3 || getWeb3()).eth.Contract)(RenExABI as AbiItem[], EXCHANGE);
+const getERC20 = (tokenAddress: string, web3?: Web3): ERC20DetailedWeb3 =>
+    new ((web3 || getWeb3()).eth.Contract)(ERC20ABI as AbiItem[], tokenAddress);
 
 /**
  * getPrice returns the rate at which dstToken can be received per srcToken.
  * @param srcToken The source token being spent
  * @param dstToken The destination token being received
  */
-const getReserveBalance = async (marketPairs: MarketPair[]): Promise<BigNumber[]> => {
-    const exchange = getExchange();
-    return marketPairs.map((_marketPair) => {
-        return new BigNumber(0);
-    });
+const getReserveBalance = async (marketPairs: MarketPair[]): Promise<Array<Map<Token, BigNumber>>> => {
+    const web3 = getWeb3();
+    const exchange = getExchange(web3);
+    return Promise.all(
+        marketPairs.map(async (_marketPair) => {
+            const [left, right] = _marketPair.split("/") as [Token, Token];
+            const leftAddress = left;
+            const rightAddress = right;
+            const reserve = await exchange.methods.reserve(leftAddress, rightAddress).call();
+            const leftToken = getERC20(leftAddress, web3);
+            const leftBalance = new BigNumber((await leftToken.methods.balanceOf(reserve).call()).toString());
+            const rightToken = getERC20(rightAddress, web3);
+            const rightBalance = new BigNumber((await rightToken.methods.balanceOf(reserve).call()).toString());
+            return new Map([[left, leftBalance], [right, rightBalance]]);
+        })
+    );
 };
 
 /**
