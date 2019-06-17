a17fd684835a2d9bb9aeb388530be7c16e2e6dda
Vince Au <vinceau09@gmail.com>
2019-06-17 15:23:14 +1000

    Remove currency dropdown from header

--- client/src/components/views/HeaderController.tsx
+++ client/src/components/views/HeaderController.tsx
@@ -1,25 +1,12 @@
 import * as React from "react";
 
-import { currencies, CurrencyIcon, Dropdown, Header } from "@renex/react-components";
+import { Header } from "@renex/react-components";
 import { Link } from "react-router-dom";
 
 import { AppContainer } from "../../state/appContainer";
 import { connect, ConnectedProps } from "../../state/connect";
 import { ReactComponent as Logo } from "../../styles/images/logo.svg";
 import { AccountDropdown } from "./AccountDropdown";
-
-const currencyOptions = (() => {
-    const options = new Map<string, React.ReactNode>();
-
-    for (const currency of currencies) {
-        options.set(currency.currency, <>
-            <CurrencyIcon currency={currency.currency} />
-            {" "}{currency.description}
-        </>);
-    }
-
-    return options;
-})();
 
 const logo = <Link className="no-underline" to="/">
     <Logo />
@@ -36,24 +23,10 @@ interface Props extends ConnectedProps<[AppContainer]> {
  */
 export const HeaderController = (connect<Props>([AppContainer])(
     ({ handleLogout, handleLogin, containers: [appContainer] }) => {
-        const quoteCurrency = appContainer.state.preferredCurrency;
-        const currencyDropdown = <Dropdown
-            key="currencyDropdown"
-            selected={{
-                value: quoteCurrency,
-                render: <>
-                    <CurrencyIcon currency={quoteCurrency} />
-                    {" "}{quoteCurrency.toUpperCase()}
-                </>
-            }}
-            options={currencyOptions}
-            setValue={appContainer.setCurrency}
-        />;
-
         return <Header
             logo={logo}
             menu={[
-                currencyDropdown, <AccountDropdown
+                <AccountDropdown
                     key="AccountDropdown"
                     handleLogin={handleLogin}
                     handleLogout={handleLogout}
