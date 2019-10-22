f5bc9aef3836f85ce60ff42c62112ee6f7cbc3f2
susruth <susruth@susruth.com>
2019-10-22 18:21:26 +1100

    Add generate go bindings script

---
+++ ethereum-contracts/contracts/Bindings.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.8;
+
+import "./DEX.sol";
+import "./DEXAdapter.sol";
+import "./DEXReserve.sol";
+
+/// @notice Bindings imports all of the contracts for generating bindings.
+/* solium-disable-next-line no-empty-blocks */
+contract Bindings {}
