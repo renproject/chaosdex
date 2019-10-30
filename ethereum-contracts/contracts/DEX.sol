248cf50c3586c290031b795e9b62c82e808afb35
noiach <gh@noiach.com>
2019-10-30 14:08:29 +1100

    contracts: added eth to recoverTokens

--- ethereum-contracts/contracts/DEX.sol
+++ ethereum-contracts/contracts/DEX.sol
@@ -26,9 +26,14 @@ contract DEX is Ownable {
         baseToken = _baseToken;
     }
 
-    /// @notice Allow anyone to recover funds accidentally sent to the contract.
+    /// @notice Allow the owner to recover funds accidentally sent to the
+    /// contract. To withdraw ETH, the token should be set to `0x0`.
     function recoverTokens(address _token) external onlyOwner {
-        ERC20(_token).transfer(msg.sender, ERC20(_token).balanceOf(address(this)));
+        if (_token == address(0x0)) {
+            msg.sender.transfer(address(this).balance);
+        } else {
+            ERC20(_token).transfer(msg.sender, ERC20(_token).balanceOf(address(this)));
+        }
     }
 
     /// @notice The DEX operator is able to register new reserves.
